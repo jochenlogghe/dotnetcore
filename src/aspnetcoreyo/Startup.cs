bd33c80de322373b5da134bf671384bba9afe9e5
LOGGHE Jochen <jlgg@picanol.be>
2016-06-22 09:12:27 +0200

    test

--- src/aspnetcoreyo/Startup.cs
+++ src/aspnetcoreyo/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -22,7 +18,7 @@ namespace aspnetcoreyo
         {
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World From Jochen L!");
             });
         }
     }
